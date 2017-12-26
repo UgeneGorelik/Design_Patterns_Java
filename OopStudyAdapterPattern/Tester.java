c2fa704cf8e91dce60cb185536da20ab3affe840
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2017-12-26 13:50:17 +0200

    Update Tester.java

--- OopStudyAdapterPattern/Tester.java
+++ OopStudyAdapterPattern/Tester.java
@@ -2,19 +2,12 @@ package OopStudyAdapterPattern;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		
-			
-			enemyTank e7=new enemyTank();
-			 e7.fireWeapon();
-			 enemyrobot a1=new enemyrobot();  
-			 
-			 EnemyrobotAdapter Erobot=new EnemyrobotAdapter(a1);
+	public static void main(String[] args) {			
+	    		 enemyTank et=new enemyTank();
+			 et.fireWeapon();
+			 enemyrobot er=new enemyrobot();  
+		         EnemyrobotAdapter Erobot=new EnemyrobotAdapter(er);
 			 Erobot.fireWeapon();
-			
-		
-
-
-	}
+		}
 
 }
