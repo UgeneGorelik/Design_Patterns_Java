48cce7bdac0d88ef67caae9435afe3cf5f6f3f82
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2017-12-26 14:08:57 +0200

    Update PatternTest.java

--- OopStudyIteratorPattern/PatternTest.java
+++ OopStudyIteratorPattern/PatternTest.java
@@ -5,7 +5,6 @@ public class PatternTest {
 	public static void main(String[] args) {
 	SongIterator r = new RockSongs();
 	SongIterator p = new PopSongs();
-	
 	DiskJocey dj = new DiskJocey(r, p);
 	dj.ShowSongs();
 	}
