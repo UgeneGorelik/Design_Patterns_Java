96d6d1043d02682ab1e15afe88c5aca3c5fb1394
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2017-12-26 13:48:44 +0200

    Update PizzaMaker.java

--- OopDecoratorPattern/PizzaMaker.java
+++ OopDecoratorPattern/PizzaMaker.java
@@ -3,7 +3,6 @@ package OopDecoratorPattern;
 public class PizzaMaker {
 
 	public static void main(String[] args) {
-		  
 	Pizza basicPizza = new TomatoSauce(new Mozarella(new PlainPizza())) ;
 	basicPizza.Description();
 
